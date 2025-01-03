86d0febd64656394a7fa7287668e5236bf42bd26
Bitoskimkim <bitoskimkim@gmail.com>
2025-01-03 16:57:22 +0800

    Create helloworld-lesson.cpp

---
+++ helloworld-lesson.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Library for input and output
+using namespace std; // Use the standard namespace
+
+int main() {
+    cout << "Hello, World!" << endl; // Print message
+    return 0; // Return 0 to indicate successful execution
+}
